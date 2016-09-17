628526328586f81608d6d0d95aec11c4166bc6cf
Leonardo Costa <leolima.str@gmail.com>
2016-09-17 02:17:40 -0300

    Codigo, classe atendente

---
+++ Simulacao.py
@@ -0,0 +1,14 @@
+#@autor: Leonardo Costa
+#@autor: Marcelo Araujo
+#@autor: Luis Santos
+
+import random
+import numpy
+import math
+import matplotlib.pyplot as plt
+
+
+class Atendente(object):
+	
+	def __init__(self):
+		self.ocupada = false
